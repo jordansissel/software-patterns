261c922d7534221c76d92582aea69f5674bc5d01
Jordan Sissel <jls@semicomplete.com>
2012-05-02 01:20:03 -0700

    Shuffle it

--- retry-on-failure/ruby/example-sequel.rb
+++ retry-on-failure/ruby/example-sequel.rb
@@ -4,7 +4,9 @@ require "./lib/try"
 pg_hosts = ["pg-replica-a", "pg-replica-b", "pg-replica-c"]
 
 # Try connecting to any database host.
-database = try(pg_hosts) do |host|
+# Also, shuffle the list before attempting so that we get a more balanced
+# connection load.
+database = try(pg_hosts.shuffle) do |host|
   db = Sequel.connect("postgres://#{host}/example")
   # Sequel::Database#test_connection returns true if successful.
   # So return the db if we succeed, otherwise raises an error
