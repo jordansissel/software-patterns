289a3fcfa64c9b23aa0a7db6d6af2fd3a6482cc7
Jordan Sissel <jls@semicomplete.com>
2012-05-02 00:53:28 -0700

    Add retry-forever support.

--- retry-on-failure/ruby/lib/try.rb
+++ retry-on-failure/ruby/lib/try.rb
@@ -1,23 +1,38 @@
 # Public: try a block of code until either it succeeds or we give up.
 #
-# enumerable - an Enumerable, #each is invoked and is tried that number of times.
-# block - the block
+# enumerable - an Enumerable or omitted, #each is invoked and is tried that
+#   number of times. If this value is omitted or nil, we will try until
+#   success with no limit on the number of tries.
 #
-# Returns the return value of the block.
+# Returns the return value of the block once the block succeeds.
+# Raises the last seen exception if we run out of tries.
 #
 # Examples
 #
+#   # Try 10 times
 #   response = try(10.times) { Net::HTTP.get_response("google.com", "/") }
+#
+#   # Try many times, yielding the value of the enumeration to the block.
+#   # This allows you to try different inputs.
+#   response = try([0, 2, 4, 6]) { |val| 50 / val }
+#   
+#   Output: 
+#   Failed (divided by 0). Retrying in 0.01 seconds...
+#   => 25
+#
+#
+#   # Try forever
+#   return_value = try { ... }
 #
 # The above will try fetching http://google.com/ at most 10 times, breaking
 # after the first non-exception return.
-def try(enumerable, &block)
+def try(enumerable=nil, &block)
   if block.arity == 0
     # If the block takes no arguments, give none
     procedure = lambda { |val| block.call }
   else
     # Otherwise, pass the current 'enumerable' value to the block.
-    procedure = lambda { |val| block.call(value) }
+    procedure = lambda { |val| block.call(val) }
   end
 
   last_exception = nil
@@ -25,6 +40,17 @@ def try(enumerable, &block)
   # Retry after a sleep to be nice.
   backoff = 0.01
   backoff_max = 2.0
+
+  # Try forever if enumerable is nil.
+  if enumerable.nil?
+    enumerable = Enumerator.new do |y| 
+      a = 0
+      while true
+        a += 1
+        y << a
+      end
+    end
+  end
 
   # When 'enumerable' runs out of things, if we still haven't succeeded, we'll
   # reraise
