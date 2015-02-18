4f7a56f4288134706cba05556f5a63051ce5239d
Jordan Sissel <jls@semicomplete.com>
2015-02-17 23:31:26 -0800

    Start fiddling with a way to do randomized integration testing

--- randomized-stress-testing/ruby/example.rb
+++ randomized-stress-testing/ruby/example.rb
@@ -10,6 +10,19 @@ end
 describe TCPServer do
   subject(:socket) { Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0) }
   let(:sockaddr) { Socket.sockaddr_in(port, "127.0.0.1") }
+  let(:ignore_eaddrinuse) do
+    proc do |m, *args|
+      begin
+        m.call(*args)
+      rescue Errno::EADDRINUSE
+        # ignore
+      end
+    end
+  end
+
+  after do
+    socket.close
+  end
 
   context "on privileged ports" do
     let(:port) { Randomized.number(1..1023) }
@@ -21,15 +34,9 @@ describe TCPServer do
   context "on unprivileged ports" do
     let(:port) { Randomized.number(1025..65535) }
     stress_it "should bind on a port" do
-      # Ignore Errno::EADDRINUSE
-      allow(socket).to(receive(:bind).and_wrap_original do |m, *args|
-        begin
-          m.call(*args)
-        rescue Errno::EADDRINUSE
-          # ignore
-        end
-      end)
-
+      # EADDRINUSE is expected since we are picking ports at random
+      # Let's ignore this specific exception
+      allow(socket).to(receive(:bind).and_wrap_original(&ignore_eaddrinuse))
       expect { socket.bind(sockaddr) }.to_not(raise_error)
     end
   end
@@ -38,6 +45,50 @@ describe TCPServer do
     let(:port) { 0 }
     stress_it "should bind successfully" do
       expect { socket.bind(sockaddr) }.to_not(raise_error)
+    end
+  end
+end
+
+describe "integration for TCPServer and TCPSocket" do
+  subject(:server) { Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0) }
+  subject(:client) { Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0) }
+  let(:sockaddr) { Socket.sockaddr_in(port, "127.0.0.1") }
+  let(:ignore_eaddrinuse) do
+    proc do |m, *args|
+      begin
+        m.call(*args)
+      rescue Errno::EADDRINUSE
+        # ignore
+      end
+    end
+  end
+
+  let(:text) { Randomized.text(1..10000) }
+  let(:port) { Randomized.number(1024..65536) }
+
+  after  do
+    server.close
+    client.close
+  end
+
+  require "pry"
+  binding.pry
+  it "should pass text" do
+    begin
+      server.bind(sockaddr)
+    rescue Errno::EADDRINUSE
+      next # Skip
+    end
+    server.listen(5)
+    client.connect(sockaddr)
+    s, _ = server.accept
+    begin
+      expect(client.syswrite(text)).to(be == text.bytesize)
+      client.close
+      data = s.read
+      expect(data).to(be == text)
+    ensure
+      s.close
     end
   end
 end
