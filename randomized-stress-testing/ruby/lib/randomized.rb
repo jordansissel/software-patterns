0ad4e23ff87f6b72479b5f94a82de3c41365cf37
Jordan Sissel <jls@semicomplete.com>
2015-02-17 23:05:06 -0800

    Add more documentation comments
    Also slight style changes

--- randomized-stress-testing/ruby/lib/randomized.rb
+++ randomized-stress-testing/ruby/lib/randomized.rb
@@ -1,9 +1,12 @@
-
+# A collection of methods intended for use in randomized testing.
 module Randomized
-
-  # Randomized text:
-  # * length (or length range)
-  # * text (valid characters)
+  # Generates text with random characters of a given length (or within a length range)
+  #
+  # * The length can be a number or a range `x..y`. If a range, it must be ascending (x < y)
+  # * Negative lengths are not permitted and will raise an ArgumentError
+  #
+  # @param length [Fixnum or Range] the length of text to generate
+  # @return [String] the 
   def self.text(length)
     if length.is_a?(Range)
       raise ArgumentError, "Requires ascending range, you gave #{length}." if length.end < length.begin
@@ -15,8 +18,11 @@ module Randomized
     end
 
     length.times.collect { character }.join
-  end
+  end # def text
 
+  # Generates a random character (A string of length 1)
+  #
+  # @return [String]
   def self.character
     # TODO(sissel): Add support to generate valid UTF-8. I started reading
     # Unicode 7 (http://www.unicode.org/versions/Unicode7.0.0/) and after much
@@ -26,13 +32,22 @@ module Randomized
     
     # Until UTF-8 is supported, just return a random lower ASCII character
     number(32..127).chr
-  end
+  end # def character
 
+  # Return a random number within a given range.
+  #
+  # @param range [Range]
   def self.number(range)
+    raise ArgumentError, "Range not given, got #{range.class}: #{range.inspect}" if !range.is_a?(Range)
     rand(range)
-  end
-
+  end # def number
+   
+  # Run a block a random number of times.
+  #
+  # @param range [Fixnum of Range] same meaning as #number(range)
   def self.iterations(range, &block)
+    range = 0..range if range.is_a?(Numeric)
     number(range).times(&block)
-  end
+    nil
+  end # def iterations
 end
--- randomized-stress-testing/ruby/lib/rspec/stress_it.rb
+++ randomized-stress-testing/ruby/lib/rspec/stress_it.rb
@@ -1,19 +1,32 @@
-
 require "rspec/core"
 
 module RSpec::StressIt
   DEFAULT_ITERATIONS = 1..1000
+
+  # Wraps `it` and runs the block many times. Each run has will clear the `let` cache.
+  #
+  # The intent of this is to allow randomized testing for fuzzing and stress testing
+  # of APIs to help find edge cases and weird behavior.
+  #
+  # The default number of iterations is randomly selected between 1 and 1000 inclusive
+  # You can control the number of iterations by setting `let(:stress_iterations) { ... }` yourself.
   def stress_it(*args, &block)
     it(*args) do
       # Run the block of an example many times
       # You can control the iteration count with `let(:stress_iterations) { ... }`
-      (respond_to?(:stress_iterations) ? stress_iterations : Randomized.number(DEFAULT_ITERATIONS)).times do
+      if respond_to?(:stress_iterations)
+        stress_iterations
+      else
+        Randomized.number(DEFAULT_ITERATIONS)
+      end.times do
+        # Run the block within 'it' scope
         instance_eval(&block)
 
         # clear the internal rspec `let` cache this lets us run a test
-        # repeatedly with new `let` evaluations
+        # repeatedly with fresh `let` evaluations.
+        # Reference: https://github.com/rspec/rspec-core/blob/5fc29a15b9af9dc1c9815e278caca869c4769767/lib/rspec/core/memoized_helpers.rb#L124-L127
         @__memoized = {}
       end
-    end
-  end
-end
+    end # it ...
+  end # def stress_it
+end # module RSpec::StressIt
